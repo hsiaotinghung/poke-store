350406a54d14cc561db203d2711e535b4c12af6f
Hsiao-Ting Hung <hsiao-tinghung@Hsiao-Tings-MBP.fios-router.home>
2017-05-17 00:38:43 -0400

    add mock pokemons

--- src/app/mock-pokemons.ts
+++ src/app/mock-pokemons.ts
@@ -73,4 +73,252 @@ export const POKEMONS: Pokemon[] = [
         height: 1.6,
         weight: 85.5
     },
+    {
+        id: 10,
+        name: 'Caterpie',
+        imgUrl: 'assets/img/GO010.png',
+        type: ['bug'],
+        height: 0.3,
+        weight: 2.9
+    },
+    {
+        id: 11,
+        name: 'Metapod',
+        imgUrl: 'assets/img/GO011.png',
+        type: ['bug'],
+        height: 0.7,
+        weight: 9.9
+    },
+    {
+        id: 12,
+        name: 'Butterfree',
+        imgUrl: 'assets/img/GO012.png',
+        type: ['bug', 'flying'],
+        height: 1.1,
+        weight: 32.0
+    },
+    {
+        id: 13,
+        name: 'Weedle',
+        imgUrl: 'assets/img/GO013.png',
+        type: ['bug', 'poison'],
+        height: 0.3,
+        weight: 3.2
+    },
+    {
+        id: 14,
+        name: 'Kakuna',
+        imgUrl: 'assets/img/GO014.png',
+        type: ['bug', 'poison'],
+        height: 0.6,
+        weight: 10.0
+    },
+    {
+        id: 15,
+        name: 'Beedrill',
+        imgUrl: 'assets/img/GO015.png',
+        type: ['bug', 'poison'],
+        height: 1.0,
+        weight: 29.5
+    },
+    {
+        id: 16,
+        name: 'Pidgey',
+        imgUrl: 'assets/img/GO016.png',
+        type: ['normal', 'flying'],
+        height: 0.3,
+        weight: 1.8
+    },
+    {
+        id: 17,
+        name: 'Pidgeotto',
+        imgUrl: 'assets/img/GO017.png',
+        type: ['normal', 'flying'],
+        height: 1.1,
+        weight: 30.0
+    },
+    {
+        id: 18,
+        name: 'Pidgeot',
+        imgUrl: 'assets/img/GO018.png',
+        type: ['normal', 'flying'],
+        height: 1.5,
+        weight: 39.5
+    },
+    {
+        id: 19,
+        name: 'Rattata',
+        imgUrl: 'assets/img/GO019.png',
+        type: ['normal'],
+        height: 0.3,
+        weight: 3.5
+    },
+    {
+        id: 20,
+        name: 'Raticate',
+        imgUrl: 'assets/img/GO020.png',
+        type: ['normal'],
+        height: 0.7,
+        weight: 18.5
+    },
+    {
+        id: 21,
+        name: 'Spearow',
+        imgUrl: 'assets/img/GO021.png',
+        type: ['normal', 'flying'],
+        height: 0.3,
+        weight: 2.0
+    },
+    {
+        id: 22,
+        name: 'Fearow',
+        imgUrl: 'assets/img/GO022.png',
+        type: ['normal', 'flying'],
+        height: 1.2,
+        weight: 38.0
+    },
+    {
+        id: 23,
+        name: 'Ekans',
+        imgUrl: 'assets/img/GO023.png',
+        type: ['poison'],
+        height: 2.0,
+        weight: 6.9
+    },
+    {
+        id: 24,
+        name: 'Arbok',
+        imgUrl: 'assets/img/GO024.png',
+        type: ['poison'],
+        height: 3.5,
+        weight: 65.0
+    },
+    {
+        id: 25,
+        name: 'Pikachu',
+        imgUrl: 'assets/img/GO025.png',
+        type: ['Electric'],
+        height: 0.4,
+        weight: 6.0
+    },
+    {
+        id: 26,
+        name: 'Raichu',
+        imgUrl: 'assets/img/GO026.png',
+        type: ['Electric'],
+        height: 0.8,
+        weight: 30.0
+    },
+    {
+        id: 27,
+        name: 'Sandshrew',
+        imgUrl: 'assets/img/GO027.png',
+        type: ['ground'],
+        height: 0.6,
+        weight: 12.0
+    },
+    {
+        id: 28,
+        name: 'Sandshrew',
+        imgUrl: 'assets/img/GO028.png',
+        type: ['ground'],
+        height: 1.0,
+        weight: 29.5
+    },
+    {
+        id: 29,
+        name: 'Nidoran',
+        imgUrl: 'assets/img/GO029.png',
+        type: ['poison'],
+        height: 0.4,
+        weight: 7
+    },
+    {
+        id: 30,
+        name: 'Nidorina',
+        imgUrl: 'assets/img/GO030.png',
+        type: ['poison'],
+        height: 0.8,
+        weight: 22
+    },
+    {
+        id: 31,
+        name: 'Nidoqueen',
+        imgUrl: 'assets/img/GO031.png',
+        type: ['poison', 'ground'],
+        height: 1.3,
+        weight: 60
+    },
+    {
+        id: 32,
+        name: 'Nidoran',
+        imgUrl: 'assets/img/GO032.png',
+        type: ['poison'],
+        height: 0.5,
+        weight: 9
+    },
+    {
+        id: 33,
+        name: 'Nidorino',
+        imgUrl: 'assets/img/GO033.png',
+        type: ['poison'],
+        height: 0.9,
+        weight: 19.5
+    },
+    {
+        id: 34,
+        name: 'Nidoking',
+        imgUrl: 'assets/img/GO034.png',
+        type: ['poison', 'ground'],
+        height: 1.4,
+        weight: 62.0
+    },
+    {
+        id: 35,
+        name: 'Clefairy',
+        imgUrl: 'assets/img/GO035.png',
+        type: ['fairy'],
+        height: 0.6,
+        weight: 7.5
+    },
+    {
+        id: 36,
+        name: 'Clefable',
+        imgUrl: 'assets/img/GO036.png',
+        type: ['fairy'],
+        height: 1.3,
+        weight: 40.0
+    },
+    {
+        id: 37,
+        name: 'Vulpix',
+        imgUrl: 'assets/img/GO037.png',
+        type: ['fire'],
+        height: 0.6,
+        weight: 9.9
+    },
+    {
+        id: 38,
+        name: 'Ninetales',
+        imgUrl: 'assets/img/GO038.png',
+        type: ['fire'],
+        height: 1.1,
+        weight: 19.9
+    },
+    {
+        id: 39,
+        name: 'Jigglypuff',
+        imgUrl: 'assets/img/GO039.png',
+        type: ['normal', 'fairy'],
+        height: 0.5,
+        weight: 5.5
+    },
+    {
+        id: 40,
+        name: 'Wigglytuff',
+        imgUrl: 'assets/img/GO040.png',
+        type: ['normal', 'fairy'],
+        height: 1.0,
+        weight: 12.0
+    }
 ];
